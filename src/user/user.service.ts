b68f90852d4c0f482077fe2bce9ab2961c68ac98
TehGaa <syosuasaputra1010@gmail.com>
2023-08-05 15:09:37 +0700

    feat: add prisma and user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getAllUser(): Promise<User[]> {
+    return await this.prismaService.user.findMany();
+  }
+}
