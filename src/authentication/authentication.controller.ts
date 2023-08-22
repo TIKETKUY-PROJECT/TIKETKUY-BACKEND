42f6bc7eeb4eb806d397663fa850a8585131cc02
TehGaa <syosuasaputra1010@gmail.com>
2023-08-22 17:57:05 +0700

    feat: add authentication api

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthRequest } from './dto';
+import { AuthenticationService } from './authentication.service';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private authService: AuthenticationService) {}
+
+  @Post('login')
+  async login(@Body() dto: AuthRequest) {
+    return await this.authService.login(dto);
+  }
+
+  @Post('signup')
+  async signup(@Body() dto: AuthRequest) {
+    return await this.authService.signUp(dto);
+  }
+}
---
+++ src/authentication/strategy/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      ignoreExpiration: false,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        id: payload.id,
+        createdAt: payload.createdAt,
+        email: payload.email,
+      },
+    });
+    if (!user)
+      throw new UnauthorizedException(
+        "User doesn't have access to this information!",
+      );
+    return payload;
+  }
+}
