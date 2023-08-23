1579431e64fda9ab280bf71d261b52c33247772c
TehGaa <syosuasaputra1010@gmail.com>
2023-08-23 14:33:35 +0700

    refactor: move jwtmodule to auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('all')
+  @UseGuards(AuthGuard('jwt'))
   async findAllUser(): Promise<any> {
     return await this.userService.findAllUser();
   }
